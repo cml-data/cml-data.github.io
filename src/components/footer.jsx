9b68251a0af75e1f90fdc9930e6f61d039ff4259
Christina Lawrence <christina.lawrence@christinas-mbp.lan>
2024-07-14 20:10:18 -0700

    fixing footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,13 @@ const Footer = () => {
             <hr />
             <div className="footer-container">
                 <p>
-                    Christy's Portfolio - 2024
+                    <div>
+                        Portfolio code based on:&nbsp;
+                        <a href="https://github.com/olawanlejoel/Kinsta-developer-portfolio">
+                            https://github.com/olawanlejoel/Kinsta-developer-portfolio
+                        </a>
+                    </div>
+                    <center>Christy's Portfolio - 2024</center>
                 </p>
             </div>
         </>
